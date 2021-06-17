cafcd315fb1c65568eacb71349086b23a1fda263
Hans-Christoph Steiner <hans@eds.org>
2021-06-17 21:19:48 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+from setuptools import Command
+from setuptools import setup
+import subprocess
+import sys
+
+
+class VersionCheckCommand(Command):
+    """Make sure git tag and version match before uploading"""
+
+    user_options = []
+
+    def initialize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def finalize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def run(self):
+        version = self.distribution.get_version()
+        version_git = (
+            subprocess.check_output(['git', 'describe', '--tags', '--always'])
+            .rstrip()
+            .decode('utf-8')
+        )
+        if version != version_git:
+            print(
+                'ERROR: Release version mismatch! setup.py (%s) does not match git (%s)'
+                % (version, version_git)
+            )
+            sys.exit(1)
+        print('Upload using: twine upload --sign dist/sdkmanager-%s.tar.gz' % version)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sdkmanager',
+    version='0.1',
+    description='Android SDK Manager',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='The F-Droid Project',
+    author_email='team@f-droid.org',
+    url='https://f-droid.org',
+    license='AGPL-3.0',
+    scripts=['sdkmanager.py'],
+    python_requires='>=3.5',
+    cmdclass={'versioncheck': VersionCheckCommand},
+    install_requires=[
+        'argcomplete',
+        'requests > 2.12.2, != 2.18.0',
+        'requests-cache >= 0.4.13',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Topic :: Utilities',
+    ],
+)
