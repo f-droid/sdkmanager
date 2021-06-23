d4e8ea8faf0ab84225ca2a247922724f640f693d
Hans-Christoph Steiner <hans@eds.org>
2021-06-23 19:38:25 +0200

    version 0.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sdkmanager',
-    version='0.1',
+    version='0.2',
     description='Android SDK Manager',
     long_description=long_description,
     long_description_content_type='text/markdown',
