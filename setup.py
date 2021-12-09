b2799fb702fbfd6405745c002abd7e100657f9f5
Hans-Christoph Steiner <hans@eds.org>
2021-12-09 20:14:08 +0100

    verson 0.4.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sdkmanager',
-    version='0.4',
+    version='0.4.1',
     description='Android SDK Manager',
     long_description=long_description,
     long_description_content_type='text/markdown',
